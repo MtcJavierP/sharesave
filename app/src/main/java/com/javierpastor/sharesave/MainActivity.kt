84b0eac32aad60961e2b801f33a439324def39f2
Javi <fjavier.pastor@ciclosmontecastelo.com>
2024-01-08 23:07:47 +0100

    Tarea3_FranciscoJavierPastorPerez Completada

--- app/src/main/java/com/javierpastor/sharesave/MainActivity.kt
+++ app/src/main/java/com/javierpastor/sharesave/MainActivity.kt
@@ -3,13 +3,39 @@ package com.javierpastor.sharesave
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.javierpastor.sharesave.ui.theme.SharesaveTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +48,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                  //  Greeting("Android")
+                    ScaffoldExample()
+
                 }
             }
         }
@@ -42,5 +70,93 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     SharesaveTheme {
         Greeting("Android")
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldExample() {
+    var presses by remember { mutableIntStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                    text="Share",
+                    modifier= Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Save",
+                )
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { presses++ }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                """
+                   Bienvenidos a Share & Save! Por favor, compartid vuestras gangas 
+                   en los distintos supermercados con el resto de usuarios para que 
+                   toda la comunidad pueda ahorrar!
+
+                    Has presionado el boton $presses veces.
+                """.trimIndent(),
+
+
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.monedas), // Reemplaza con el ID de tu imagen
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp) // Ajusta la altura según sea necesario
+                    .clip(shape = MaterialTheme.shapes.medium)
+            )
+
+            LazyColumn {
+                items(listOf("-  Supermercados", "-  Ofertas", "-  Productos", "-  Ciudad")) { item ->
+                    Text(
+                        text = item,
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .clickable {
+                                // Lógica para manejar el clic en el elemento
+                                // Puedes navegar a otra pantalla, realizar alguna acción, etc.
+                                // Por ahora, puedes imprimir un mensaje en la consola
+                                println("Elemento clickeado: $item")
+                            }
+                    )
+                }
+            }
+        }
     }
 }
